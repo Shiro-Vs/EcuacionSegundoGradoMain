cb28a584efb9ad33c6bb55998309c916e842b907
ShiroVs <robert.vs@outlook.com>
2026-04-29 15:14:56 -0500

    Version 2: correccion del signo de la formula general

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Versión 1: Uso de enteros, error de signo y sin validaciones
+        // Version 2: correccion del signo de la formula general
         int a, b, c;
         double x1, x2;
 
@@ -19,8 +19,8 @@ public class Main {
         System.out.print("Ingrese c: ");
         c = sc.nextInt();
 
-        x1 = (b + Math.sqrt((b * b) - (4 * a * c))) / (2 * a);
-        x2 = (b - Math.sqrt((b * b) - (4 * a * c))) / (2 * a);
+        x1 = (-b + Math.sqrt((b * b) - (4 * a * c))) / (2 * a);
+        x2 = (-b - Math.sqrt((b * b) - (4 * a * c))) / (2 * a);
 
         System.out.println("Raiz 1: " + x1);
         System.out.println("Raiz 2: " + x2);
